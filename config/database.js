b66a3739cf81487eda4094f56ac7f438c7b5b21e
shelawd <shelawdya44@gmail.com>
2023-12-17 21:58:43 +0700

    finish backend

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('upload_db','root','',{
+const db = new Sequelize('mikro_id','root','',{
     host: 'localhost',
     dialect: "mysql"
 });
